61c6b8038e88d9d414bb6149ed7f01fadbf29853
tepremy <remymtep@gmail.com>
2022-12-04 13:20:51 +0100

    fix custom cursor

--- components/CustomCursor.jsx
+++ components/CustomCursor.jsx
@@ -66,7 +66,7 @@ const CustomCursor = () => {
     followMouse();
   }, []);
   return (
-    <div className={`cursor-wrapper` + " z-100 hidden xl:block"}>
+    <div className={`cursor-wrapper` + " z-100"}>
       <div className="main-cursor " ref={mainCursor}>
         <div className="main-cursor-background"></div>
       </div>
